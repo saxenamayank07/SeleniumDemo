d9cbcb8bb6582d0e42b19fab22a4e68cee64f21a
saxenamayank07 <saxenamayank07@gmail.com>
2022-04-07 13:30:41 +0530

    Initial Commit

---
+++ src/test/java/seleniumJava/DemoSeleniumProject/Test01.java
@@ -0,0 +1,18 @@
+package seleniumJava.DemoSeleniumProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test01 {
+	@Test
+	public void firstTest() {
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.gmail.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+}
